3adb90184d80e895f3b52bbbcb28e489f88ec845
MAXSCRIPT <lukuanenko@gmail.com>
2024-08-19 17:48:09 +0300

    1.6.6

---
+++ gltfpack.bat
@@ -0,0 +1,24 @@
+:: GLTF Packer
+:: 1.0.0
+:: Vasyl Lukianenko 
+:: 3DGROUND
+:: https://3dground.net
+
+echo off
+cls
+
+set input=%1
+set gltf=
+set glb=
+
+for /f "tokens=1,2,3 delims=;" %%i in (%input%) do (
+	set gltf=%%i
+	set glb=%%j
+)
+
+gltfpack.exe -vtf -vnf -vpf -vc 16 -vn 16 -vp 16 -vt 16 -tc -tl 2048 -i "%gltf%" -o "%glb%"
+
+for %%a in ("%gltf%") do set "folder=%%~dpa"
+if not "%folder%"=="" (
+	rmdir /s /q "%folder%"
+)
